78cf07a3db9bbe51ad865eaaf7c23cfc3b865b3d
[Aditya <[www.aditya117@gmail.com]>
2024-05-11 10:53:01 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,33 @@
-import { useState, useEffect, useRef } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import VideoPlayer from './VideoPlayer';
+import { useState, useEffect, useRef } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import VideoPlayer from "./VideoPlayer";
 
 function App() {
   const playerRef = useRef(null);
-  const [videoLink, setVideoLink] = useState('');
+  const [videoLink, setVideoLink] = useState("");
 
-  useEffect(() => {
-    fetch('localhost:8000/uploads')
-      .then(response => response.json())
-      .then(data => {
-        setVideoLink(data.videoUrl);
-      })
-      .catch(error => {
-        console.error('Error fetching video link:', error);
-      });
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    try {
+      const response = await fetch(
+        "http://localhost:8000/uploads",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const data = await response.json();
+      console.log("data",data)
+      setVideoLink(data?.videoUrl)
+    } catch (error) {
+      setMessage(`Error: ${error.message}`);
+    }
+  };
+
 
   const videoPlayerOptions = {
     controls: true,
@@ -26,28 +36,41 @@ function App() {
     sources: [
       {
         src: videoLink,
-        type: 'application/x-mpegURL'
-      }
-    ]
+        type: "application/x-mpegURL",
+      },
+    ],
   };
 
-  const handlePlayerReady = player => {
+  const handlePlayerReady = (player) => {
     playerRef.current = player;
 
-    player.on('waiting', () => {
-      console.log('Player is waiting');
+    player.on("waiting", () => {
+      console.log("Player is waiting");
     });
 
-    player.on('dispose', () => {
-      console.log('Player will dispose');
+    player.on("dispose", () => {
+      console.log("Player will dispose");
     });
   };
+
+  const handlePlay = () => {};
 
   return (
     <>
+    <form name="fileInputForm" onSubmit={handleSubmit}>
+      <input
+        type="file"
+        name="file"
+        accept="video/*"
+        style={{}}
+        // onChange={handleFileChange}
+      />
+      <input type="submit" value="Submit"/>
+      </form>
       <div>
         <h1>Video player</h1>
       </div>
+      <button onClick={() => handlePlay()}>Play</button>
       <VideoPlayer options={videoPlayerOptions} onReady={handlePlayerReady} />
     </>
   );
